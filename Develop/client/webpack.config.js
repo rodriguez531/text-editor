3d5d57116c78afbde1d64a562127d9abf194ec02
rodriguez531 <alexr5408@gmail.com>
2022-10-17 17:12:31 -0400

    updating repo with concurrenlty and idb

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
